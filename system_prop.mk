f5dee7890a32b16c66e26fa268294de1f151f0e3
ZIM <zim555@mail.ru>
2017-01-11 20:06:43 +0200

    Revert "Try use opensource GPS"
    This reverts commit 6985236604943ee0005a198d1ab502ef71fd50bc.

--- system_prop.mk
+++ system_prop.mk
@@ -2,7 +2,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=1 \
     ro.gps.agps_provider=1 \
-    persist.loc.nlp_name=com.qualcomm.location
+    ro.qc.sdk.izat.premium_enabled=0 \
+    ro.qc.sdk.izat.service_mask=0x0
 
 # Bluetooth properties
 PRODUCT_PROPERTY_OVERRIDES += \
