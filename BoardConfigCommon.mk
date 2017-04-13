d828ec0ead03b20d5e2d20c7d8dd24dcd3ba8ad1
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-04-13 18:27:05 -0500

    fortunaxx-common: Allow F2FS userimage

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,6 +172,7 @@ TARGET_SCREEN_HEIGHT                 := 960
 # Recovery
 TARGET_RECOVERY_FSTAB                := $(LOCAL_PATH)/rootdir/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4           := true
+TARGET_USERIMAGES_USE_F2FS 	     := true
 BOARD_HAS_LARGE_FILESYSTEM           := true
 TARGET_RECOVERY_DENSITY              := hdpi
 BOARD_HAS_NO_MISC_PARTITION          := true
