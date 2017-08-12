1e0377130c83a148b841a82154caa5b5f0ea2563
ZIM <zeustreker@gmail.com>
2017-08-12 14:48:36 +0300

    Decrease minimal scaling freq. to 200Mhz
    Update interactive governor tunables
     Set the interactive target_loads tunable to lower settings so that
     the cpu is not constantly ramped up at or above 800Mhz.
    
    From:
    target_loads: 1 800000:85 998400:90 1094400:80
    To:
    target_loads: 1 200000:40 400000:50 533333:70 800000:82 998400:90 1094400:95 1209600:99

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -494,7 +494,7 @@ case "$target" in
                 echo 0 > /sys/module/msm_thermal/core_control/enabled
 		echo 1 > /sys/devices/system/cpu/cpu0/online
                 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+                echo 200000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
                 # enable thermal core_control now
                 echo 1 > /sys/module/msm_thermal/core_control/enabled
 
@@ -539,9 +539,9 @@ case "$target" in
                        echo 25000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
                        echo 998400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
                        echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
-                       echo "1 800000:85 998400:90 1094400:80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                       echo "1 200000:40 400000:50 533333:70 800000:82 998400:90 1094400:95 1209600:99" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
                        echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
-                       echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
+                       echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis
                        echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
                        ;;
                 esac
