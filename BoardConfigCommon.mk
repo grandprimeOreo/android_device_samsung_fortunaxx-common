46aa007977c8611219f3e057ec73b871538e42b4
ZIM <zeustreker@gmail.com>
2017-10-20 15:43:35 +0300

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,8 +135,8 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE       := true
 
 # Power HAL
 #TARGET_POWERHAL_SET_INTERACTIVE_EXT  := $(LOCAL_PATH)/power/power_ext.c
-TARGET_POWERHAL_VARIANT              := none
-WITH_QC_PERF                         := false
+TARGET_POWERHAL_VARIANT              := qcom
+WITH_QC_PERF                         := true
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH      := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
