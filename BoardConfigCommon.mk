1de640f22977b18bf7c17aaed9cf4a34aac36002
ZIM <zeustreker@gmail.com>
2017-11-07 16:00:17 +0200

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,7 +114,7 @@ MALLOC_SVELTE                        := true
 
 # Audio
 #BOARD_USES_ALSA_AUDIO                := true
-USE_CUSTOM_AUDIO_POLICY              := 1
+#USE_CUSTOM_AUDIO_POLICY              := 1
 #TARGET_QCOM_AUDIO_VARIANT            := caf
 #TARGET_USES_QCOM_MM_AUDIO            := true
 
