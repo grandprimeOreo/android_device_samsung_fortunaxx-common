0c90d6fc9e6101db69fe50be5cbabe5f5e24c2bf
ZIM <zeustreker@gmail.com>
2018-01-13 20:02:36 +0200

    Update

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,6 +123,7 @@ BOARD_USES_ALSA_AUDIO                := true
 USE_CUSTOM_AUDIO_POLICY              := 1
 TARGET_QCOM_AUDIO_VARIANT            := caf
 TARGET_USES_QCOM_MM_AUDIO            := true
+BOARD_USES_GENERIC_AUDIO             := true
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND         := true
